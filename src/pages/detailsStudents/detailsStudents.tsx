ce204c5c575bd4bd568c7e41a2834c6ee958529b
Davi Santana <santananogueira13@hotmail.com>
2023-08-08 21:10:12 -0300

    add query of student in backend

--- src/pages/detailsStudents/detailsStudents.tsx
+++ src/pages/detailsStudents/detailsStudents.tsx
@@ -1,15 +1,47 @@
 import BaseLayout from "../../layouts/baseLayoutPage";
 import DetailsTools from "../../components/detailsTools/detailsTools";
 import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { studentsService } from "../../services/studentsService/studentsService";
 
 
 function DetailsStudents(){
     const { id } = useParams();
     const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+    const [studentName, setStudentName] = useState('');
+
+    useEffect(() => {
+      if(id !== 'new'){
+        setIsLoading(true);
+        studentsService.getById(Number(id)).then((result) => {
+          setIsLoading(false);
+          if(result instanceof Error){
+            alert(result.message);
+            navigate('/students');
+          } else {
+            setStudentName(result.name);
+            console.log(result);
+          }
+        })
+      }
+    },[id]);
+
+    const handleDelete = (id:number) => {
+      if(window.confirm('Realmente deseja apagar esse registro?')){
+        studentsService.deleteById(id).then((result) => {
+          if(result instanceof Error){
+            alert(result.message); 
+          } else {
+            navigate('/students');
+          }
+        })
+    }
+  }
 
   return(
     <BaseLayout 
-    title="Informações do aluno"
+    title={id === 'new' ? 'Cadastrar novo aluno' : studentName}
     toolbar={
         <DetailsTools
         showSaveButton
@@ -17,7 +49,7 @@ function DetailsStudents(){
         showDeleteButton={id !== 'new'}
         onClickAddButton={() => navigate('/students/details/new')}
         onClickBackButton={() => navigate(-1)}
-        onClickDeleteButton={() => {}}
+        onClickDeleteButton={() => handleDelete(Number(id))}
         onClickSaveButton={() => {}}
         />
     }>
